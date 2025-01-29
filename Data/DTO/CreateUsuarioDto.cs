23db51c4c2393e56932756e4477dfcb698bf27a6
Ighor Rhafael <ighorrhafael@hotmail.com>
2025-01-29 15:10:09 +0000

    Criando Projeto

--- Data/DTO/CreateUsuarioDto.cs
+++ Data/DTO/CreateUsuarioDto.cs
@@ -6,11 +6,14 @@ public class CreateUsuarioDto
 {
     [Required]
     public required string UserNome { get; set; }
+
     [Required]
     public DateTime DataNascimento { get; set; }
+
     [Required]
     [DataType(DataType.Password)]
     public required string Password { get; set; }
+
     [Required]
     [Compare("Password")]
     public string? ConfirmPassword { get; set; }
